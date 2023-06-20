4932e8c6153d15c48b895f50612110a288f5beac
Sarath Menon <sarathmenon@mailbox.org>
2023-06-20 13:54:45 +0200

    update

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='pyscal_rdf',
+    name='pyscal_rdf_gui',
     version='0.0.4',
     author='Abril Azocar Guzman, Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Ontology based structural manipulation and quering',
     long_description=readme,
     long_description_content_type='text/markdown',
-    packages=find_packages(include=['pyscal_rdf', 'pyscal_rdf.*']),
+    packages=find_packages(include=['pyscal_rdf_gui', 'pyscal_rdf_gui.*']),
     zip_safe=False,
-    download_url = 'https://github.com/pyscal/pyscal_rdf',
+    download_url = 'https://github.com/pyscal/pyscal_rdf_gui',
     url = 'https://pyscal.org',
     install_requires=['numpy', 'ase', 'rdflib', 
     'pyyaml', 'graphviz', 'networkx', 
